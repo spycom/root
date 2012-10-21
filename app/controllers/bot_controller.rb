cbdf89e51d0faa4d8ba74cc394ce8a3a9701cd7e
Ivan Kataitsev <ivan.kataitsev@gmail.com>
2012-10-21 14:01:08 +0400

    instance var here (do not use global vars, always, almost)

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,6 +1,6 @@
 class BotController < ApplicationController
   def index
-	$user = User.new name:"cron", exp:0, level:1, money:20, job:0, hdpts:1, sfpts:0
- 	$user.save
+  	@user = User.new name:"cron", exp:0, level:1, money:20, job:0, hdpts:1, sfpts:0
+   	@user.save
   end
 end
